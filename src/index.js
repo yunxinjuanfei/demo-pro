e3d6ce6060ee191fdb0dec232ee1f0efb2ebbd70
yunxinjuanfei <yunxinjuanfei@126.com>
2019-01-24 17:13:11 +0800

    初见

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-//导入评论项子组件
+//导入子组件
 import TopHeader from '@/components/header/topheader'
 import BotFooter from '@/components/footer/botfooter'
 import HomeIndex from '@/route/home/home'
@@ -12,7 +12,6 @@ import HomeIndex from '@/route/home/home'
 import '@/zsqy_pc_public.css'
 import '@/css/zsqy_pc_style.css'
 
-//引入插件
 
 //3.调用render函数渲染,jsx比XML比HTML严格多了
 ReactDOM.render(<div className="zsqy_pc_conent">
