82a8212d5e754078046a9d37d43b0edd1d85e70b
yunxinjuanfei <yunxinjuanfei@126.com>
2019-01-25 15:19:43 +0800

    初见

--- src/components/header/topheader.jsx
+++ src/components/header/topheader.jsx
@@ -1,43 +1,50 @@
 import React from 'react'
 
 //logo
-import Logo from '@/assets/images/logo1.png'
+import Logo from '@/assets/images/logo.png'
+import Logo1 from '@/assets/images/logo1.png'
 
 //官网头部导航区
 export default class TopHeader extends React.Component {
     constructor() {
         super()
         this.state = {
-            index:'首页',
-            product:'产品中心',
-            news:'新闻中心',
-            download:'移动端APP',
-            about:'关于我们'
+            top: false 
         }
-        this.handleScroll = this.handleScroll.bind(this)
     }
     componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
+        window.addEventListener('scroll', this.handleScroll.bind(this))
+    }
+    componentDidUpdate() {
+        window.addEventListener('scroll', this.handleScroll.bind(this))
     }
     componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
+        window.removeEventListener('scroll', this.handleScroll.bind(this))
     }
-    handleScroll(e) {
-        console.log('浏览器滚动事件');
+    //导航吸顶
+    handleScroll(ev) {
+        let HT = ev.srcElement;
+        var top = HT.documentElement.scrollTop || HT.body.scrollTop;
+        console.log(top, this.state)
+        if (this.state && ((!!top) !== this.state.top)) {
+            this.setState({
+                top: (!!top)
+            })
+        }
     }
 
     render() {
-        return <div className="zsqy_index_header"> 
+        return <div className={(!this.state.top) ? "zsqy_index_header" : "zsqy_index_header header_bj"}> 
             <div className="header_content1 clearfix">
                 <div className="logo fl">
-                    <img src={Logo} alt="" />
+                    <img src={(!this.state.top) ? Logo1 : Logo} alt="" />
                 </div>
-                <div className="header_nav  fr">
-                    <a href="#">{this.state.index}</a>
-                    <a href="product.html">{this.state.product}</a>
-                    <a href="news.html">{this.state.news}</a>
-                    <a href="download_app.html">{this.state.download}</a>
-                    <a href="aboutUs.html">{this.state.about}</a>
+                <div className={(!this.state.top) ? "header_nav fr" : "header_nav item fr"}>
+                    <a href="#" className={(this.state.top) ? "on" : ''}>首页</a>
+                    <a href="product.html">产品中心</a>
+                    <a href="news.html">新闻中心</a>
+                    <a href="download_app.html">APP下载</a>
+                    <a href="aboutUs.html">关于我们</a>
                 </div>
             </div>
         </div>
